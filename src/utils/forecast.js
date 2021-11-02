1ed21ad04737e232a9f5056eef4ec56966c98053
Mallari, Cathleen Joyce <cathleen.j.mallari@accenture.com>
2021-11-02 17:21:57 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
-    }
+            console.log(body.current.weather_descriptions[0])
+            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
+        }
 })
 }
 module.exports = forecast
